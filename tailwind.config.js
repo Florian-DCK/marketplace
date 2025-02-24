0e9b2248468a67231e03da577d407fa86a44a9c2
Florian Donckers <fdonckersf@gmail.com>
2025-02-24 10:23:41 +0100

    added intellisense tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+	theme: {
+		extend: {
+			colors: {
+				fond: '#5242ae',
+			},
+			fontFamily: {
+				sans: ['Helvetica', 'Arial', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
