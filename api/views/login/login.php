3bb9a8acd0235c07ae5d4dad783bc79f50ea5144
Opura <apeangopura@gmail.com>
2025-02-24 13:18:48 +0100

    login signup work in progress

--- api/views/login/login.php
+++ api/views/login/login.php
@@ -6,11 +6,11 @@
     </div>
     <div class="flex mb-8 p-2 rounded-sm bg-[#EAEBED] w-md">
         <span class="flex items-center pl-2"><img src="../../public/svg/email.svg" alt="email_logo" class="w-5 h-5"></span>
-        <input type="text" name="email" class="p-1 text-[#9F9F9F]" placeholder="Email">
+        <input type="text" name="email" class="p-1 text-[#9F9F9F] w-full focus:outline-none" placeholder="Email">
     </div>
     <div class="flex mb-8 p-2 rounded-sm bg-[#EAEBED] w-md">
         <span class="flex items-center pl-2"><img src="../../public/svg/closed-lock-password.svg" alt="password_logo" class="w-5 h-5"></span>
-        <input type="text" name="password" class="p-1 text-[#9F9F9F]" placeholder="Password">
+        <input type="password" name="password" class="p-1 text-[#9F9F9F] w-full focus:outline-none" placeholder="Password">
     </div>
-    <button type="submit" class="cursor-pointer flex justify-center font-semibold text-xl text-white border rounded-full w-60 py-3 hover:bg-white hover:text-[#FFD1A9] duration-500 ease-in-out hover:scale-115">Log in</button>   
+    <button type="submit" class="cursor-pointer flex justify-center font-semibold text-xl bg-white text-[#FFD1A9] rounded-full w-60 py-3 hover:bg-[#FFD1A9] hover:text-white hover:border duration-500 ease-in-out hover:scale-115">Log in</button>   
 </form>
