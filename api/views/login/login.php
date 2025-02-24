37bbe875242c6ced9b95c7da6b698552cd4a3e03
Opura <apeangopura@gmail.com>
2025-02-24 13:28:36 +0100

    border fix

--- api/views/login/login.php
+++ api/views/login/login.php
@@ -12,5 +12,5 @@
         <span class="flex items-center pl-2"><img src="../../public/svg/closed-lock-password.svg" alt="password_logo" class="w-5 h-5"></span>
         <input type="password" name="password" class="p-1 text-[#9F9F9F] w-full focus:outline-none" placeholder="Password">
     </div>
-    <button type="submit" class="cursor-pointer flex justify-center font-semibold text-xl bg-white text-[#FFD1A9] rounded-full w-60 py-3 hover:bg-[#FFD1A9] hover:text-white hover:border duration-500 ease-in-out hover:scale-115">Log in</button>   
+    <button type="submit" class="cursor-pointer flex justify-center font-semibold text-xl bg-white text-[#FFD1A9] rounded-full w-60 py-3 hover:bg-[#FFD1A9] hover:text-white duration-500 ease-in-out hover:scale-115 border">Log in</button>   
 </form>
