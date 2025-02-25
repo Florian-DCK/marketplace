b82f28af5114e7983c1f54945c4d4ca7b85411a9
Florian Donckers <fdonckersf@gmail.com>
2025-02-25 20:31:35 +0100

    added shaodw to navbar

--- api/views/navbar/categoryNavbar.php
+++ api/views/navbar/categoryNavbar.php
@@ -22,7 +22,7 @@ $categories = [
 ?>
 
 <div class="bg-[#EAEBED] w-full flex justify-center">
-    <ul class="flex space-x-2 items-center w-5/6 justify-center">
+    <ul class="flex space-x-2 items-center w-[90%] justify-center shadow-[0_10px_10px_-10px_rgba(0,0,0,0.1)]">
         <?php
             foreach ($categories as $category) {
                 echo "<a href='"."/search?category=".$category."'><li class='p-2 hover:bg-gray-200 cursor-pointer'>". $category ."</li> </a>";
