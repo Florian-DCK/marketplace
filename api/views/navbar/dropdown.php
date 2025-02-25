f464916a40d11e0b467767cd850158243bbab183
Florian Donckers <fdonckersf@gmail.com>
2025-02-25 19:39:38 +0100

    changed to using inline svgs

--- api/views/navbar/dropdown.php
+++ api/views/navbar/dropdown.php
@@ -1,6 +1,6 @@
     
 <div>
-    <img id="menu-button" src="/api/public/svgs/hamburger.svg" alt="Menu" class="size-6 cursor-pointer hover:bg-gray-300 rounded-md">
+    <svg id="menu-button" xmlns="http://www.w3.org/2000/svg" fill="none" class="size-6 cursor-pointer hover:bg-gray-300 rounded-md stroke-black" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-width="1.5" d="M20 7H4M20 12H4M20 17H4"/></svg>
     <div id="menu-dropdown" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 focus:outline-hidden hidden transform opacity-0 scale-95 transition-all duration-100 ease-in-out" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
         <!-- Active: "bg-gray-100 outline-hidden", Not Active: "" -->
         <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
