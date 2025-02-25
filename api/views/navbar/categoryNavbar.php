6959e5d3c48d00ab25cb220445f1258e1b2ee828
Florian Donckers <fdonckersf@gmail.com>
2025-02-25 20:07:31 +0100

    changed color of background

--- api/views/navbar/categoryNavbar.php
+++ api/views/navbar/categoryNavbar.php
@@ -1,4 +1,32 @@
 <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 
+<?php
 
-<div class=" bg-blue-400 w-screen">Catégories</div>
+$categories = [
+    "Food",
+    "Tech",
+    "Furnitures",
+    "Books",
+    "Sports",
+    "Crafts",
+    "Pets",
+    "Human slavery",
+    "Cars",
+    "Clothes",
+    "Beauty, healthcare and wellness",
+    "Kitchen and houses",
+]
+
+
+
+?>
+
+<div class="bg-[#EAEBED] w-full flex justify-center">
+    <ul class="flex space-x-2 items-center w-5/6 justify-center">
+        <?php
+            foreach ($categories as $category) {
+                echo "<li class='p-2 hover:bg-gray-200 cursor-pointer'>". $category ."</li>";
+            }
+        ?>
+    </ul>
+</div>
