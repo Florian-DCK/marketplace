48e0a1bba6aa2ed3a1eff272c85cda25cf11667d
Opura <apeangopura@gmail.com>
2025-02-28 11:16:20 +0100

    responsive work in progress

--- api/views/login/signIn-signUp.php
+++ api/views/login/signIn-signUp.php
@@ -7,16 +7,16 @@
 </head>
 <body>
     <div class="flex justify-center items-center h-screen w-screen">
-        <div class="w-full flex">
+        <div class="w-full flex flex-col md:flex-row"> <!-- Log in dominant -->
             <?php include_once 'login.php'; ?>
-            <div name="switch-to-signup" class="w-1/2 flex flex-col items-center my-auto font-semibold tracking-tight text-3xl text-[#FFD1A9]">
+            <div name="switch-to-signup" class="w-1/2 flex flex-col items-center mx-auto whitespace-nowrap font-semibold tracking-tight text-3xl text-[#FFD1A9]">
                 <p class="mb-5">Welcome !</p>
                 <p class="mb-5">Pour créer votre compte,</p>
                 <p class="mb-5">c'est par ici !</p>
                 <button type="submit" class="cursor-pointer flex justify-center font-semibold text-xl text-white bg-[#FFD1A9] border rounded-full w-60 py-3 hover:bg-white hover:text-[#FFD1A9] duration-500 ease-in-out hover:scale-115">Sign in</button> 
             </div>
         </div>
-        <div class="w-full hidden">
+        <div class="w-full hidden"> <!-- Sign up dominant -->
             <div name="switch-to-login" class="w-1/2 flex flex-col items-center font-semibold tracking-tight text-3xl bg-[#FFD1A9] text-white">
                 <p class="mb-5 mt-auto">Welcome back !</p>
                 <p class="mb-5">Pour continuer,</p>
