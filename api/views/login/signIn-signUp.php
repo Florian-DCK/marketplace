53f4c367dba171618799ad0115ea753bc653b660
Opura <apeangopura@gmail.com>
2025-03-10 12:53:15 +0100

    login page + logo

--- api/views/login/signIn-signUp.php
+++ api/views/login/signIn-signUp.php
@@ -5,9 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marketplace - Connexion</title>
 </head>
+
+<?php 
+	$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+?>
+
 <body>
-    <div class="flex justify-center items-center h-screen w-screen">
-        <div class="w-screen h-screen flex flex-col lg:flex-row hidden"> <!-- Log in dominant -->
+    <div class="flex justify-center items-center h-screen w-screen static">
+        <div class="absolute top-0 left-0">
+            <span class="flex items-center">
+                <div class="h-12 w-12 bg-[#5242AE] rounded-full m-3"
+                    id="avatar">
+                </div>
+                <h1 class="hidden sm:flex sm:text-3xl lg:text-5xl">Marketplace</h1>
+            </span>
+        </div>
+        <div class="w-screen h-screen flex flex-col lg:flex-row flex"> <!-- Log in dominant -->
             <?php include_once 'login.php'; ?>
             <div name="switch-to-signup" class="w-1/2 p-5 flex flex-col lg:text-xl xl:text-2xl mx-auto items-center my-auto whitespace-nowrap font-semibold tracking-tight text-3xl text-[#FFD1A9]">
                 <p class="mb-5 hidden lg:flex">Welcome !</p>
@@ -16,8 +29,8 @@
                 <button type="submit" class="cursor-pointer flex my-5 justify-center font-semibold text-xl text-white bg-[#FFD1A9] border rounded-full w-60 lg:w-50 py-3 hover:bg-white hover:text-[#FFD1A9] hover:duration-500 ease-in-out hover:scale-115">Create account</button> 
             </div>
         </div>
-        <div class="w-full h-screen flex flex-col lg:flex-row"> <!-- Sign up dominant --> <!-- pt-70 à check -->
-            <div name="switch-to-login" class="pt-5 lg:h-full flex flex-col text-center items-center justify-center px-5 font-semibold tracking-tight text-3xl bg-[#FFD1A9] text-white">
+        <div class="w-full h-screen flex flex-col lg:flex-row hidden"> <!-- Sign up dominant --> <!-- pt-70 à check -->
+            <div name="switch-to-login" class="pt-20 lg:h-full flex flex-col text-center items-center justify-center px-5 font-semibold tracking-tight text-3xl bg-[#FFD1A9] text-white">
                 <p class="mb-5 hidden lg:flex">Welcome back !</p>
                 <p class="mb-5 hidden lg:flex">Pour continuer,</p>
                 <p class="mb-5 hidden lg:flex">veuillez vous connecter !</p>
