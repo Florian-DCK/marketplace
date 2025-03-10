330d38d7c2bc1e4e1702b07884da00bad4414576
Florian Donckers <fdonckersf@gmail.com>
2025-03-10 11:30:34 +0100

    WIP

---
+++ api/views/dashboard/sidebar.php
@@ -0,0 +1,8 @@
+<body>
+    <div class=" w-full max-w-1/5">
+        <ul class="h-full w-full flex flex-col space-y-4 text-right px-5 pt-5">
+            <li><a href="">Mon profil</a></li>
+            <li><a href="">Se déconnecter</a></li>
+        </ul>
+    </div>
+</body>
