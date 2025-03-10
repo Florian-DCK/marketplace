75ce562de8c95b27135ae624ac64d0c63f3cd245
Florian Donckers <fdonckersf@gmail.com>
2025-03-10 13:45:08 +0100

    WIP

---
+++ api/views/dashboard/adminDashboard.php
@@ -0,0 +1,12 @@
+
+<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+<main class="h-screen w-screen flex flex-col">
+    <?php include __DIR__ . '/../navbar/navbar.php'; ?>
+    <div class="bg-[#EAEBED] h-full flex flex-1">
+    <?php   include __DIR__ . '/../dashboard/sidebar.php';
+            include __DIR__ . '/../dashboard/userAdd.php';
+    ?>
+    </div>
+    
+</main>
