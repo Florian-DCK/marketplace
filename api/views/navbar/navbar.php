66026a2fe7876fe4fe8665936112b1dca363aa36
Florian Donckers <fdonckersf@gmail.com>
2025-03-16 15:48:14 +0100

    Ajout de Mustache comme dépendance, création de nouveaux templates pour la barre de recherche et la navigation des catégories, et suppression des fichiers PHP obsolètes.

--- api/views/navbar/navbar.php
+++ api/views/navbar/navbar.php
@@ -1,30 +1,43 @@
 
 <?php 
-	$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
-?>
-		<div class="relative h-max w-screen bg-[#EAEBED]">
-			<div class="flex items-center justify-between">
-				<a href="/">
-				<span class="flex items-center">
 
-					<div
-						class=" h-12 w-12 bg-[#5242AE] rounded-full m-3"
-						id="avatar"></div>
-					<h1 class=" text-5xl">Marketplace</h1>
-				</span>
-			</a>
-                <?php include 'searchBar.php'; ?>
-				<div class="flex items-center mr-2 space-x-3">
-					<a href="/login"><button class=" bg-[#5242AE] text-white px-2 py-1 rounded-lg cursor-pointer">Connexion</button></a>
+require __DIR__ . '/../../../vendor/autoload.php';
 
-					<a href="/dashboard"><button class=" bg-[#5242AE] text-white px-2 py-1 rounded-lg cursor-pointer">Dashboard</button></a>
-					
-					<a href="/dashboard/admin"><button class=" bg-[#5242AE] text-white px-2 py-1 rounded-lg cursor-pointer">Admin</button></a>
-				</div>
-			</div>
-            <?php
-				if(!str_contains($url, 'dashboard')){
-					include 'categoryNavbar.php';
-				}
-			?>
-		</div>
+$mustache = new Mustache_Engine([
+	'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../../templates'),
+	'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../../templates/partials')
+]);
+
+$categories = [
+    "Food",
+    "Tech",
+    "Furnitures",
+    "Books",
+    "Sports",
+    "Crafts",
+    "Pets",
+    "Human slavery",
+    "Cars",
+    "Clothes",
+    "Beauty, healthcare and wellness",
+    "Kitchen and houses",
+];
+$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+$data = [
+	'showCategoryNavbar' => !str_contains($url, 'dashboard'),
+    'isConnected' => false,
+	'showMenu' => true,
+	'categories' => array_map(function($category) {
+		return ['name' => $category];
+	}, $categories),
+	'menuItems' => [
+		['url' => '/dashboard', 'label' => 'Your Profile', 'id' => 'user-menu-item-0'],
+		['url' => '/dashboard/admin', 'label' => 'Admin', 'id' => 'user-menu-item-1'],
+		['url' => '#', 'label' => 'Sign Out', 'id' => 'user-menu-item-2'],
+	],
+    'userName' => 'John Doe',
+    'userProfileImage' => '/api/public/defaultAvatar.jpg'
+];
+
+echo $mustache->render('navbar', $data);
