13a30c5e96fa73d8bfcc7372ed0300c8a6c92d43
Florian Donckers <fdonckersf@gmail.com>
2025-03-17 19:12:25 +0100

    try fix for session

--- api/config/session.php
+++ api/config/session.php
@@ -1,10 +1,10 @@
 <?php
 if (!function_exists('init_session')) {
     function init_session() {
-        if (getenv('VERCEL')) {
-            session_save_path('/tmp');
-        }
         if (session_status() === PHP_SESSION_NONE) {
+            if (getenv('VERCEL')) {
+                session_save_path('/tmp');
+            }
             session_start();
         }
     }
