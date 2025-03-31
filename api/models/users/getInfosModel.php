da409858f6f4aec2cb02becf0d3b06229378ade6
Bledaquest <bledaquest@gmail.com>
2025-03-31 10:23:27 +0200

    DeleteUserAccountFunction

--- api/models/users/getInfosModel.php
+++ api/models/users/getInfosModel.php
@@ -17,3 +17,43 @@
             return null;
         }
     }
+
+    function getIsActive($email, $isActive, $db) {
+        try {
+            $user  = $db->query("SELECT isActive FROM User WHERE email = :email", [':email' => $email]);
+            
+            // Si l'utilisateur existe, renvoyer ses informations
+            if ($user) {
+                $db->close(); 
+                return $isActive;
+            } else {
+                $db->close();
+                return null;
+            }
+
+            if ($isActive == 1) {
+                $db->query("UPDATE User SET isActive = 0 WHERE email = :email", [':email' => $email]);
+            } else {
+                $db->query("UPDATE User SET isActive = 1 WHERE email = :email", [':email' => $email]);
+            }
+        } catch (PDOException $e) {
+            $db->close(); 
+            echo 'Erreur de requête : ' . $e->getMessage();
+            return null;
+        }
+    }
+
+    /*function deleteUser($isActive, $email, $db) {
+        try {
+            if ($isActive == 1) {
+                $db->query("UPDATE User SET isActive = 0 WHERE email = :email", [':email' => $email]);
+            } else {
+                $db->query("UPDATE User SET isActive = 1 WHERE email = :email", [':email' => $email]);
+            }
+        } catch (PDOException $e) {
+            $db->close(); 
+            echo 'Erreur de requête : ' . $e->getMessage();
+            return null;
+        }
+
+    }*/
