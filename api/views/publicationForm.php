7c6294a429bf4afa1f6aceb77f8a4e428e11b751
herve1986 <vandendrisse.herve@gmail.com>
2025-04-14 13:49:46 +0200

    publicationFOrm

--- api/views/publicationForm.php
+++ api/views/publicationForm.php
@@ -19,41 +19,45 @@ $url = $_SERVER['REQUEST_URI'];
 </head>
 
 
-    <?php
-        include __DIR__ . '/navbar.php';
-        include __DIR__ . '/../models/database.php';
-        //include __DIR__ . '/../models/users/recupCategory.php';
+<?php
+    include __DIR__ . '/navbar.php';
+    include __DIR__ . '/../models/database.php';
 
-        $mustache = new Mustache_Engine([
-            'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates'),
-            'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates/partials')
-            ]);
+    $mustache = new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates'),
+        'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates/partials')
+        ]);
 
-            echo $mustache->render('publicationForm');
-        
-        if (!empty($_POST)) {
-            $idVendor = $_SESSION['id'];
-            $title = $_POST['title'];
-            $idCategory = $_POST['category'];
-            $price = $_POST['price'];
-            $image = $_FILES['image'];
-            $description = $_POST['description'];
-            
+        echo $mustache->render('publicationForm');
+    
+    if (!empty($_POST)) {
+        $idVendor = $_SESSION['id'];
+        $title = $_POST['title'];
+        $idCategory = $_POST['category'];
+        $price = $_POST['price'];
+        $image = $_FILES['image'];
+        $description = $_POST['description'];
+    
+        $imageResult = image_upload($image);
+        if (is_array($imageResult)) {
+    
             $conn = new connectionDB();
-
-            $conn ->query ("INSERT INTO Product (id_category, id_vendor, title, description, image, price) 
-            VALUES(:id_category,:id_vendor, :title, :description, :image, :price)", 
+    
+            $conn->query(
+                "INSERT INTO Product (id_category, id_vendor, title, description, image, price) 
+                VALUES(:id_category,:id_vendor, :title, :description, :image, :price)", 
                 [
-                ":id_category"=> $idCategory,
-                ":id_vendor"=> $idVendor,
-                ":title" => $title,
-                ":description" => $description,
-                ":image" => $image,
-                ":price" => $price
+                    ":id_category" => $idCategory,
+                    ":id_vendor" => $idVendor,
+                    ":title" => $title,
+                    ":description" => $description,
+                    ":image" => $image,
+                    ":price" => $price
                 ]
-        );
-        $conn->close();
-        return "Success";
+            );
+    
+            $conn->close();
+            echo "Success";
         }
-    ?>
-
+    }
+?>
