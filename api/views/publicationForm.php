123a8d2ca187fb6981e66d8160eb7a07b9effcb1
herve1986 <vandendrisse.herve@gmail.com>
2025-04-14 13:17:54 +0200

    form annonce

--- api/views/publicationForm.php
+++ api/views/publicationForm.php
@@ -1,4 +1,5 @@
 <?php
+session_start() ;
 require_once __DIR__ . '/../config/session.php';
 init_session();
 
@@ -19,6 +20,7 @@ $url = $_SERVER['REQUEST_URI'];
     <?php
         include __DIR__ . '/navbar.php';
         include __DIR__ . '/../models/database.php';
+        //include __DIR__ . '/../models/users/recupCategory.php';
 
         $mustache = new Mustache_Engine([
             'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates'),
@@ -26,6 +28,29 @@ $url = $_SERVER['REQUEST_URI'];
             ]);
 
             echo $mustache->render('publicationForm');
+        
+        if (!empty($_POST)) {
+            $idVendor = $_SESSION['id'];
+            $title = $_POST['title'];
+            $idCategory = $_POST['category'];
+            $price = $_POST['price'];
+            $image = $_FILES['image'];
+            $description = $_POST['description'];
+            
+            $conn = new connectionDB();
 
+            $conn ->query ("INSERT INTO Product (id_category, id_vendor, title, description, image, price) 
+            VALUES(:id_category,:id_vendor, :title, :description, :image, :price)", 
+                [
+                ":id_category"=> $idCategory,
+                ":id_vendor"=> $idVendor,
+                ":title" => $title,
+                ":description" => $description,
+                ":image" => $image,
+                ":price" => $price
+                ]
+        );
+
+        }
     ?>
 
