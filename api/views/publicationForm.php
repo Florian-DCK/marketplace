ba277bc9bd2ae4826b6a7e820b6596f7a90872dc
herve1986 <vandendrisse.herve@gmail.com>
2025-05-05 15:18:31 +0200

    produits

--- api/views/publicationForm.php
+++ api/views/publicationForm.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/session.php';
+
 init_session();
 
 $url = $_SERVER['REQUEST_URI'];
@@ -19,13 +20,57 @@ $url = $_SERVER['REQUEST_URI'];
     <?php
         include __DIR__ . '/navbar.php';
         include __DIR__ . '/../models/database.php';
-
+        $conn = new connectionDB();
+        function getAllCategories($db) {
+            try {
+                $Category = $db->query("SELECT * FROM Category");
+                
+                // Si l'article existe, renvoyer ses informations
+                if ($Category) {
+                   
+                    return $Category; 
+                } else {
+                   
+                    return null;
+                }
+            } catch (PDOException $e) {
+                echo 'Erreur de requête : ' . $e->getMessage();
+                return null;
+            }
+    };
+    $AllCategories = getAllCategories($conn);
         $mustache = new Mustache_Engine([
             'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates'),
             'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates/partials')
             ]);
+            $data = [
+                'categories' => $AllCategories,
+            ];
 
-            echo $mustache->render('publicationForm');
+            echo $mustache->render('publicationForm', $data);
 
+       
+        
+            if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST)) {
+                $id = $_SESSION['id'];
+                $title = $_POST['title'];
+                $category = $_POST['category'];
+                $price = $_POST['price'];
+                //$image = $_FILES['image'];
+                $description = $_POST['description'];
+        var_dump($id);
+    
+                $conn->query("INSERT INTO Product (id_category, id_user, title, description, price) 
+                VALUES (:id_category, :id_user, :title, :description, :price)",
+                [
+                ":id_category" => $category,
+                ":id_user" => $id, 
+                ":title" => $title,
+                ":description" => $description,
+                //":image" => $image,  
+                ":price" => $price
+                ]);
+            }
+            $conn->close();
     ?>
 
