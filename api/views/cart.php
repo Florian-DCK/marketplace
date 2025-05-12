623ef0a75f82b3aa7ec29a41fbf7ee2b0d700dfe
Bledaquest <bledaquest@gmail.com>
2025-05-12 14:32:14 +0200

    cart.php

---
+++ api/views/cart.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../config/session.php';
+init_session();
+
+$url = $_SERVER['REQUEST_URI'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketplace - Cart</title>
+    <link rel="stylesheet" href="/global.css">
+</head>
+
+<?php
+
+    include __DIR__ . '/navbar.php';
+    include __DIR__ . '/../models/database.php';
+
+    $mustache = new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates'),
+        'partials_loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../templates/partials')
+    ]);
+
+    $user_id = $_SESSION['user_id'] ?? null;
+    
+
+?>
