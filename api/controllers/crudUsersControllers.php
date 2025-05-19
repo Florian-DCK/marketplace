2a49a97910099bc3cac326bf01b402b4a17c90b9
herve1986 <vandendrisse.herve@gmail.com>
2025-05-19 15:22:00 +0200

    tentative dashboarduser

---
+++ api/controllers/crudUsersControllers.php
@@ -0,0 +1,36 @@
+
+<?php
+require_once __DIR__ . '/../config/session.php';
+init_session();
+ob_start();
+
+include __DIR__ . "/../models/users/crudUsersModel.php";
+
+// Récupérer les valeurs du formulaire
+$username = $_POST['username'];
+$firstName = $_POST['firstName'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$avatar = $_FILES['avatar'];
+
+// Appeler la fonction pour mettre à jour le nom
+$db = new connectionDB();
+
+if (!empty($_POST['username'])) {
+    updateName($db, $_SESSION['id'], $_POST['username']);
+}
+if (!empty($_POST['firstName'])) {
+    updateSurname($db, $_SESSION['id'], $_POST['firstName']);
+}
+if (!empty($_POST['email'])) {
+    updateEmail($db, $_POST['email'], $_SESSION['id']);
+}
+if (!empty($_POST['phone'])) {
+    updatePhone($db, $_POST['phone'], $_SESSION['id']);
+}
+if (!empty($_FILES['avatar']['tmp_name'])) {
+    $avatarPath = '/uploads/' . basename($_FILES['avatar']['name']);
+    move_uploaded_file($_FILES['avatar']['tmp_name'], __DIR__ . '/../../public' . $avatarPath);
+    updateAvatar($db, $_SESSION['id'], $avatarPath);
+}
+header("Location: /dashboard");
