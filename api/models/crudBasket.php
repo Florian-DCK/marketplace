5930b5ec810196a868ecca90e58b64c83ee7f554
Bledaquest <bledaquest@gmail.com>
2025-05-19 14:33:03 +0200

    lkjh

--- api/models/crudBasket.php
+++ api/models/crudBasket.php
@@ -35,7 +35,6 @@
     }
 
     function getBasket($id, $basket_id, $db) {
-        checkBasket($basket_id, $db);
         try {
             $productBasket = $db->query("SELECT * FROM ProductBasket WHERE id = :id", [
                 ':id' => $id
@@ -52,7 +51,6 @@
     }
 
     function clearBasket($id, $basket_id, $db) {
-        checkBasket($basket_id, $db);
         try {
             $db->query("DELETE * FROM productBasket WHERE id = :id", [
                 ':id' => $id
@@ -64,7 +62,6 @@
     }
 
     function deleteProduct($product_id,$basket_id ,$db) {
-        checkBasket($basket_id, $db);
         try {
             $db->query("DELETE * FROM ProductBasket WHERE product_id = :product_id", [
                 ':product_id' => $product_id
@@ -76,36 +73,11 @@
     }
 
     function updateBasket($id,$basket_id, $newQuantity, $db) {
-        checkBasket($basket_id, $db);
         try {
             $db->query("UPDATE ProductBasket SET quantity = :quantity WHERE id = :id", [
                 ':id' => $id,
                 ':quantity' => $newQuantity
             ]);
-        } catch (PDOException $e) {
-            echo 'Erreur de requête : ' . $e->getMessage();
-            return null;
-        }
-    }
-
-    function checkBasket($user_id, $db) {
-        try {
-            $basket = $db->query("SELECT * FROM Basket WHERE user_id = :user_id", [
-                ':user_id' => $user_id
-            ]);
-            if ($basket) {
-                return $basket; 
-            } else {
-                try {
-                    $db->query("INSERT INTO Basket (user_id) VALUES (:user_id)", [
-                        ':user_id' => $user_id
-                    ]);
-                }
-                catch (PDOException $e) {
-                    echo 'Erreur de requête : ' . $e->getMessage();
-                    return null;
-                }
-            }
         } catch (PDOException $e) {
             echo 'Erreur de requête : ' . $e->getMessage();
             return null;
